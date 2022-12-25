e6468d9e5c4c88b65f1046ed910e836d663c2090
Win Maw <sytreacy@gmail.com>
2022-12-26 04:23:58 +0630

    add: thanks design, inputs validation toggle, remove: tags
    add:
    - form success design with css styles
    - toggle <p>s to display error inputs
    - store inputs and display on cards
    
    remove:
    - unnecessary <div>s surrounding input fields and <p>s for displaying error inputs
    - unnecessary display none on .error-msg
    
    edit:
    - change confirm button type to "button" to stop form reloading

--- validation.js
+++ validation.js
@@ -7,13 +7,120 @@ const patterns = {
   cvc: /^[0-9]{3}$/,
 };
 
+let cardholder;
+let cardnumber;
+let expire_month;
+let expire_year;
+let cvc;
+
+const confirmBtn = document.querySelector(".confirm-btn");
+confirmBtn.disabled = true;
+confirmBtn.style.cursor = "no-drop";
+
 function validate(regex, field) {
   if (regex.test(field.value)) {
     field.className = "valid";
-    field.parentElement.parentElement.lastElementChild.style.display = "none";
+    removeError(field);
+    saveInputs(field);
+    confirmBtn.disabled = false;
+    confirmBtn.style.cursor = "pointer";
   } else {
     field.className = "invalid";
-    field.parentElement.parentElement.lastElementChild.style.display = "unset";
+    displayError(field);
+  }
+}
+
+function displayError(field) {
+  let parentDiv = field.parentElement;
+  let errorTag = document.createElement("p");
+
+  switch (field.attributes.name.value) {
+    case "cardholder":
+      if (parentDiv.children.length !== 3) {
+        errorTag.classList.add("error-msg");
+        errorTag.textContent = "characters length within 8 to 20 only";
+        parentDiv.insertBefore(errorTag, parentDiv.after());
+      }
+      break;
+
+    case "cardnumber":
+      if (parentDiv.children.length !== 3) {
+        errorTag.classList.add("error-msg");
+        errorTag.textContent = "16 numbers only";
+        parentDiv.insertBefore(errorTag, parentDiv.after());
+      }
+      break;
+
+    case "expire_month":
+    case "expire_year":
+      if (parentDiv.parentElement.children.length !== 3) {
+        errorTag.classList.add("error-msg");
+        errorTag.textContent = "Can't be blank";
+        parentDiv.parentElement.insertBefore(errorTag, parentDiv.after());
+      }
+      break;
+
+    case "cvc":
+      if (parentDiv.children.length !== 3) {
+        errorTag.classList.add("error-msg");
+        errorTag.textContent = "3 numbers only";
+        parentDiv.insertBefore(errorTag, parentDiv.after());
+      }
+      break;
+
+    default:
+      console.log("we are out of expressions..");
+  }
+}
+
+function removeError(field) {
+  let parentDiv = field.parentElement;
+  let errorMsg = document.querySelector("p.error-msg");
+
+  switch (field.attributes.name.value) {
+    case "expire_month":
+    case "expire_year":
+      if (parentDiv.parentElement.lastChild == errorMsg) {
+        parentDiv.parentElement.removeChild(errorMsg);
+      }
+      break;
+
+    default:
+      if (parentDiv.lastChild == errorMsg) {
+        parentDiv.removeChild(errorMsg);
+      }
+  }
+}
+
+function saveInputs(field) {
+  switch (field.attributes.name.value) {
+    case "cardholder":
+      cardholder = field.value;
+      console.log(cardholder);
+      break;
+
+    case "cardnumber":
+      cardnumber = field.value;
+      console.log(cardnumber);
+      break;
+
+    case "expire_month":
+      expire_month = field.value;
+      console.log(expire_month);
+      break;
+
+    case "expire_year":
+      expire_year = field.value;
+      console.log(expire_year);
+      break;
+
+    case "cvc":
+      cvc = field.value;
+      console.log(cvc);
+      break;
+
+    default:
+      console.log("total 5 inputs only");
   }
 }
 
@@ -22,3 +129,27 @@ inputs.forEach((input) => {
     validate(patterns[e.target.attributes.name.value], input);
   });
 });
+
+const form = document.getElementsByTagName("form")[0];
+const successDiv = document.querySelector(".card-success-container");
+
+function displaySavedData() {
+  addSpace = cardnumber.match(/.{1,4}/g);
+  finalCardNo = addSpace.join(" ");
+  document.querySelector(".card-back_cvc").textContent = cvc;
+  document.getElementsByClassName("cardNo")[0].textContent = finalCardNo;
+  document.querySelector(".cardholder-info > p").textContent = cardholder;
+  document.querySelector(
+    ".cardholder-info > p+p"
+  ).textContent = `${expire_month} / ${expire_year}`;
+}
+
+confirmBtn.addEventListener("click", displaySavedData);
+
+//not working yet
+// let cardNoField = document.querySelector("#cardnumber");
+// cardNoField.addEventListener("keydown", () => {
+//   if (cardNoField.value.length % 4 == 0) {
+//     cardNoField.value += " ";
+//   }
+// });
